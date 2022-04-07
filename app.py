afbb25fab1870bc127de05987d482b5f1190a12d
Michael Markin <84154105+misha345a@users.noreply.github.com>
2022-04-07 16:31:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ error_messages = {'$DELETE_PUNCT': 'Deleted incorrectly placed punctuation.',
 
 @st.cache(suppress_st_warning=True, allow_output_mutation=True, show_spinner=True)
 def load_ner_model():
-    model_name = 'xlm-roberta-large-finetuned-conll03-english
+    model_name = 'xlm-roberta-large-finetuned-conll03-english'
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForTokenClassification.from_pretrained(model_name)
     nlp_pipe = pipeline(task='ner', model=model, tokenizer=tokenizer, framework='pt')
